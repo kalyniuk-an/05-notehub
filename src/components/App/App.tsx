d17c75cc8ce3f35a94a45b0f3fc6eaff76c5d61c
Anastasiia Kalyniuk <Kalyniuk.an@gmail.com>
2026-08-09 15:50:31 +0300

    paginate

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,22 +1,47 @@
 import { useQuery} from '@tanstack/react-query'
+import { useState, type ComponentType } from 'react'
 
 import css from './App.module.css'
 import NoteList from '../NoteList/NoteList'
 import SearchBox from '../SearchBox/SearchBox'
-import {fetchNotes, type FetchNotesResponse } from '../../services/noteServise'
+import { fetchNotes, type FetchNotesResponse } from '../../services/noteServise'
+import ReactPaginateModule from "react-paginate";
+import type { ReactPaginateProps } from "react-paginate";
+
+
+
+type ModuleWithDefault<T> = { default: T };
+
+const ReactPaginate = (
+  ReactPaginateModule as unknown as ModuleWithDefault<ComponentType<ReactPaginateProps>>
+).default;
 
 export default function App() {
-  const { data } = useQuery<FetchNotesResponse>({
-    queryKey: ['notes'],
-    queryFn: () => fetchNotes({ page: 1, perPage: 12 })
+  const [page, setPage] = useState(1);
+  const { data, isSuccess } = useQuery<FetchNotesResponse>({
+    queryKey: ['notes', page],
+    queryFn: () => fetchNotes({ page, perPage: 12 })
     // placeholderData: keepPreviousData
   });
 
+  const totalPages = data?.totalPages ?? 0;
   return (
     <div className={css.App}>
       <header className={css.toolbar}>
         {<SearchBox />}
-        {/*pagination*/}
+        {isSuccess && totalPages > 1 && (
+          <ReactPaginate
+            pageCount={totalPages}
+            pageRangeDisplayed={3}
+            marginPagesDisplayed={1}
+            onPageChange={({ selected }) => { setPage(selected + 1); }}
+            forcePage={page - 1}
+            containerClassName={css.pagination}
+            activeClassName={css.active}
+            nextLabel=">"
+            previousLabel="<"
+          />
+        )}
         {/*create note button*/}
       </header>
       <NoteList notes={data?.notes || []} />
