95588a624ed6c5b91f727f14e5e84f4b90e2067e
Anastasiia Kalyniuk <Kalyniuk.an@gmail.com>
2026-08-10 22:35:29 +0300

    search +/-

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,14 @@
 import { useQuery} from '@tanstack/react-query'
 import { useState, type ComponentType } from 'react'
 
+
 import css from './App.module.css'
 import NoteList from '../NoteList/NoteList'
 import SearchBox from '../SearchBox/SearchBox'
 import { fetchNotes, type FetchNotesResponse } from '../../services/noteServise'
 import ReactPaginateModule from "react-paginate";
 import type { ReactPaginateProps } from "react-paginate";
-
-
+// import { useDebounce } from 'use-debounce'
 
 type ModuleWithDefault<T> = { default: T };
 
@@ -16,19 +16,31 @@ const ReactPaginate = (
   ReactPaginateModule as unknown as ModuleWithDefault<ComponentType<ReactPaginateProps>>
 ).default;
 
+const PER_PAGE = 12;
+
 export default function App() {
+  const [search, setSearch] = useState('');
   const [page, setPage] = useState(1);
+
+  // const [debouncedSearch] = useDebounce(search, 500);
   const { data, isSuccess } = useQuery<FetchNotesResponse>({
-    queryKey: ['notes', page],
-    queryFn: () => fetchNotes({ page, perPage: 12 })
+    queryKey: ['notes', page, PER_PAGE, search],
+    queryFn: () => fetchNotes({ page, perPage: PER_PAGE, search }),
     // placeholderData: keepPreviousData
   });
 
   const totalPages = data?.totalPages ?? 0;
+
+  const handleSearch = (value: string) => {
+    setSearch(value);
+    setPage(1);
+    console.log('Search value:', value);
+  };
+
   return (
     <div className={css.App}>
       <header className={css.toolbar}>
-        {<SearchBox />}
+        {<SearchBox value={search} onSearch={handleSearch} />}
         {isSuccess && totalPages > 1 && (
           <ReactPaginate
             pageCount={totalPages}
--- src/components/SearchBox/SearchBox.tsx
+++ src/components/SearchBox/SearchBox.tsx
@@ -1,11 +1,18 @@
 import css from './SearchBox.module.css'
 
-export default function SearchBox() {
+interface SearchBoxProps {
+  value: string;
+  onSearch: (value: string) => void;
+}
+
+export default function SearchBox({ value, onSearch }: SearchBoxProps) {
   return (
     <input
       className={css.input}
       type="text"
       placeholder="Search notes"
+      value={value}
+      onChange={(e) => onSearch(e.target.value)}
     />
   )
 }
